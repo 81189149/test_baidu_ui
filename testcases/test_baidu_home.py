773658e7301c37c08cb0b177d756ba77e27f6009
团圆 <81189149@qq.com>
2026-03-04 17:51:00 +0800

    优化了allure_html并喝了三瓶冰阔落

--- testcases/test_baidu_home.py
+++ testcases/test_baidu_home.py
@@ -1,16 +1,17 @@
 import allure
 
 
+
 @allure.feature("示例功能")
 @allure.story("打开网站")
-@allure.title("验证 example.com 页面标题")
+@allure.title("验证 baidu.com 页面标题")
 def test_example(page):
 
     with allure.step("打开网站"):
-        page.goto("https://example.com")
+        page.goto("https://www.baidu.com")
 
     with allure.step("获取标题"):
         title = page.title()
 
     with allure.step("断言标题"):
-        assert "Example" in title
+        assert "百度" in title
