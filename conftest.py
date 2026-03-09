ea59aad88608d8346151f87b389308942579b7d0
jack <81189149@qq.com>
2026-03-09 17:57:32 +0800

    截图测试4

--- conftest.py
+++ conftest.py
@@ -5,7 +5,31 @@ from PIL import ImageGrab
 from allure_commons.types import AttachmentType
 
 
+# print("🔥🔥🔥 红红火火恍恍惚惚 🔥🔥🔥")
+# @pytest.hookimpl(hookwrapper=True)
+# def pytest_runtest_makereport(item, call):
+#     # 让 pytest 先正常执行
+#     outcome = yield
+#     report = outcome.get_result()
+#
+#     # 只在“用例执行阶段失败”时截图
+#     if report.when == "call" and report.failed:
+#         print("🔥 断言失败，准备截图")  # ← 用来验证 hook 是否进来了
+#         try:
+#             img = ImageGrab.grab()  # 截全屏
+#             buffer = io.BytesIO()
+#             img.save(buffer, format="PNG")  # 转成 PNG 字节
+#
+#             allure.attach(
+#                 buffer.getvalue(),
+#                 name="失败截图",
+#                 attachment_type=AttachmentType.PNG
+#             )
+#         except Exception as e:
+#             print("❌ 截图失败：", e)
+
 print("🔥🔥🔥 红红火火恍恍惚惚 🔥🔥🔥")
+
 @pytest.hookimpl(hookwrapper=True)
 def pytest_runtest_makereport(item, call):
     # 让 pytest 先正常执行
@@ -16,12 +40,11 @@ def pytest_runtest_makereport(item, call):
     if report.when == "call" and report.failed:
         print("🔥 断言失败，准备截图")  # ← 用来验证 hook 是否进来了
         try:
-            img = ImageGrab.grab()  # 截全屏
-            buffer = io.BytesIO()
-            img.save(buffer, format="PNG")  # 转成 PNG 字节
+            page = item.funcargs.get("page")  # 获取 playwright 的 page
+            buffer = page.screenshot()  # 直接截图（CI也能用）
 
             allure.attach(
-                buffer.getvalue(),
+                buffer,
                 name="失败截图",
                 attachment_type=AttachmentType.PNG
             )
