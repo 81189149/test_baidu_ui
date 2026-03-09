fecbf5fd8c787218c13a5d8a282d39c25a6924c3
jack <81189149@qq.com>
2026-03-09 17:46:38 +0800

    截图测试

--- conftest.py
+++ conftest.py
@@ -1,41 +1,8 @@
 import io
-
 import allure
 import pytest
 from PIL import ImageGrab
 from allure_commons.types import AttachmentType
-from playwright.sync_api import sync_playwright
-
-from LBUI.tools.Global_Tools import get_page_by_url
-
-
-@pytest.fixture(scope="session")
-def playwright_instance():
-    """Playwright 全局实例"""
-    with sync_playwright() as p:
-        yield p
-
-@pytest.fixture(scope="session")
-def browser(playwright_instance):
-    """浏览器（CDP 连接已打开 Chrome）"""
-    browser = playwright_instance.chromium.connect_over_cdp(
-        "http://localhost:9222"
-    )
-    yield browser
-
-@pytest.fixture(scope="function")
-def context(browser):
-    """context"""
-    context = browser.contexts[0]
-    yield context
-
-@pytest.fixture(scope="function")
-def page(context):
-    """主页"""
-    page = get_page_by_url(context, "/app/a6bc5827/#/home")
-    assert page is not None, "未找到主页 Page"
-    page.bring_to_front()
-    yield page
 
 
 print("🔥🔥🔥 红红火火恍恍惚惚 🔥🔥🔥")
